0ff273d3e46d3c6ea369b771f89a81520628af73
saurabh <saurabh.k@ehostinguk.com>
2024-02-29 01:35:27 +0530

    allergy completed

--- components/screens/components/AllergyItem.js
+++ components/screens/components/AllergyItem.js
@@ -2,16 +2,20 @@ import { View, Text, TouchableOpacity } from "react-native"
 import { Card } from "react-native-paper";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const AllergyItem = ({data}) =>{
+const AllergyItem = ({data, deleteAllergy}) =>{
     return(
         <Card style={pahrmecyItemStyle}>
             <View style={wrapperStyle}>
                 <View style={starStyle}>
-                    <Text>Drug Name : </Text>
-                    <Text>Strength : </Text>
+                    <Text>{data.medical_allergies}</Text>
+                    <Text>{data.severity_name}</Text>
+                    <View style={bottomStyle}>
+                        <Text style={halfWidth}>{data.reaction_type}</Text>
+                        <Text style={halfWidth}>{data.notes}</Text>
+                    </View>
                 </View>
                 <View>
-                    <TouchableOpacity onPress={()=>{}}>
+                    <TouchableOpacity onPress={()=>deleteAllergy(data.id)}>
                         <Icon name="trash" size={20} color="red" />
                     </TouchableOpacity>
                 </View>
@@ -24,3 +28,5 @@ export default AllergyItem;
 const pahrmecyItemStyle = {paddingHorizontal: 16, paddingVertical: 8, paddingBottom: 10, backgroundColor: '#fff', marginHorizontal: 15, marginBottom: 12, borderRadius: 8}
 const wrapperStyle = {flexDirection: 'row'}
 const starStyle={flex: 1}
+const bottomStyle = {flexDirection: 'row', justifyContent: 'space-between'}
+const halfWidth = {width:'42%'}
